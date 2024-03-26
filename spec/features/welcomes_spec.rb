e0f06da54b61d15024ba8a6c211ff8efe91e16a8
AnaPerola <anaperolinha@hotmail.com>
2024-03-26 09:52:09 -0300

    Fix errors

--- spec/features/welcomes_spec.rb
+++ spec/features/welcomes_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-feature 'Welcomes', type: :feature do
+feature 'Welcome', type: :feature do
   scenario 'Mostra a mensagem de Bem-vindo' do
     visit(root_path)
     expect(page).to have_content('Bem-vindo')
